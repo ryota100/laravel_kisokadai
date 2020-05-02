7e083fe3c3145d4fadd582b3ccb6e4279304b26d
EC2 Default User <ec2-user@ip-172-31-17-139.us-east-2.compute.internal>
2020-05-02 04:42:54 +0000

    Laravel課題4 モデルの実装 テストデータの作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 
     <div class="container mt-4">
-        <div class="card mt-4">
-            <div class="card-header mb-2">
-                Title
+        @foreach ($posts as $post)
+            <div class="card mt-4">
+                <div class="card-header mb-2">
+                     {{ $post->title }}
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        {{$post->body}}
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <span class="mr-2">
+                        投稿日時
+                    </span>
+                </div>
             </div>
-            <div class="card-body">
-                <p class="card-text">
-                    body
-                </p>
-            </div>
-            <div class="card-footer">
-                <span class="mr-2">
-                    投稿日時
-                </span>
-            </div>
-        </div>
+        @endforeach
+        
     </div>
     
 @endsection('content')
